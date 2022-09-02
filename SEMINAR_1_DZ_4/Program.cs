4007251a767d80630dab50a74ad21f87d5b4521f
Natalya <job-nnn@mail.ru>
2022-09-02 16:49:52 +0300

    Задание 4

---
+++ SEMINAR_1_DZ_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N),  
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write ("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i=1;
+System.Console.WriteLine($"Четные числа в диапазоне от 1 до {N}:");
+while (i<=N)
+{
+if (i%2==0) System.Console.Write($"{i} ");
+ i++;  
+}
+
+ 
