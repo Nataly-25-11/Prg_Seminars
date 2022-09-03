bf8d077fdd063458cc7e2f91cf013126b66a3661
Natalya <job-nnn@mail.ru>
2022-09-03 17:52:54 +0300

    SEMINAR_2_DZ_2

---
+++ SEMINAR_2_DZ_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+Console.Write ("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int a3=(a%1000-a%100)/100;
+if (a<100) {System.Console.WriteLine("Третьeй цифры нет.");}
+else {System.Console.WriteLine($"Третья цифра: {a3}");} 
