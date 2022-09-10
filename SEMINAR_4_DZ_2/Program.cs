f32c8e2f76dd8249c3ca9a8c5a3b41c68409e24a
Natalya <job-nnn@mail.ru>
2022-09-10 22:00:52 +0300

    SEMINAR_4_DZ_2

---
+++ SEMINAR_4_DZ_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10 
+// 9012 -> 12
+int SumNum (int x){
+int sum = 0;
+int i = 1;
+int z = x;
+    while (z > 0)
+    {
+        sum=sum + x /Convert.ToInt32(Math.Pow(10,i-1))%10;
+        z/=10;
+        i=i+1;
+    }
+return sum;
+}
+System.Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма цифр в числе {A} равна {SumNum(A)}");
