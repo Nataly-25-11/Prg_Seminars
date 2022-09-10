9ed56b74eb373e96d9a7e22bf1c85e55ca25a464
Natalya <job-nnn@mail.ru>
2022-09-10 22:03:47 +0300

    SEMINAR_4_DZ_3

---
+++ SEMINAR_4_DZ_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GetArray(){
+    int[] result = new int[8];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(100);
+    }
+    return result;
+}
+void PrintArray(int[] arr){
+    System.Console.WriteLine("Массив:");
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+        if (i< arr.Length-1) {System.Console.Write(", ");            
+        } else  System.Console.WriteLine("]");
+    }   
+}
+
+PrintArray(GetArray());
