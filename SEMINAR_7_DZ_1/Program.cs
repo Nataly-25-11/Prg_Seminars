42b4ce51e55aaa51b52b4b47b867f34ba67a35fd
Natalya <job-nnn@mail.ru>
2022-09-22 13:46:16 +0300

    SEMINAR_7_DZ_1

---
+++ SEMINAR_7_DZ_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+double[,] GenerateArrayDouble(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArrayMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i,j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+double[,] myArray = GenerateArrayDouble(3, 4);
+PrintArrayMatrix(myArray);
