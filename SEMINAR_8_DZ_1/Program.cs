82a540176b24700ba0ecb2cab575a268ec334f7c
Natalya <job-nnn@mail.ru>
2022-09-25 18:14:10 +0300

    SEMINAR_8_DZ_1

---
+++ SEMINAR_8_DZ_1/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая
+ упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+int[,] GenerateArray(int rows, int columns, int minRnd, int maxRnd)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minRnd, maxRnd + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArrayMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j],4}\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] SortMassMaXMiN(int[,] massiv)
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1) - 1; j++)
+        {
+            int PozMaxChisla = j;
+            for (int k = j + 1; k < massiv.GetLength(1); k++)
+            {
+                if (massiv[i, k] > massiv[i, PozMaxChisla])
+                {
+                    PozMaxChisla = k;
+                }
+            }
+            int byfer = massiv[i, j];
+            massiv[i, j] = massiv[i, PozMaxChisla];
+            massiv[i, PozMaxChisla] = byfer;
+        }
+    }
+    return massiv;
+}
+
+int[,] myarray = GenerateArray(rows: 3, columns: 4, minRnd: 0, maxRnd: 10);
+System.Console.WriteLine("Исходный массив: ");
+PrintArrayMatrix(myarray);
+System.Console.WriteLine("Массив, в котором упорядочены по убыванию" +
+" элементы каждой строки: ");
+PrintArrayMatrix(SortMassMaXMiN(myarray));
